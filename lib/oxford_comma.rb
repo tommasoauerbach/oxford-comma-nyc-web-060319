2e4f8fe877124792bd1a9085b4872b6de6742f04
tommaso auerbach <tsa302@nyu.edu>
2019-06-03 00:25:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 
 def oxford_comma(array)
-  fruits = ["kiwi"].join
-end
-
-def oxford(array)
-  fruits = ["kiwi", "durian"].join(and)
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end
